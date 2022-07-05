7995a38195b1e946d20b4166b388212582a8e450
hanshen <han.shen@thoughtworks.com>
2022-07-05 18:12:26 +0800

    update: the newest version.

--- src/views/repo/repo-popup/popup-controller.js
+++ src/views/repo/repo-popup/popup-controller.js
@@ -3,21 +3,21 @@ const Controller = function ($scope, $uibModalInstance, $state, repo) {
   $scope.id = repo.id;
   $scope.title = repo.title;
   $scope.author = repo.author;
-  // $scope.selectedOption = $scope.id;
 
   $scope.cancel = function () {
+    // $uibModalInstance.dismiss('cancel');
+    // 使用dismiss报警告
     $uibModalInstance.close();
   };
 
-  $scope.update = function (inputContent){
-
-    $scope.repo = angular.copy(inputContent);
+//submit的逻辑：把修改后的内容 传递给后端 跳转至详情页
+  $scope.submit = function (repo){
+    $scope.repo = angular.copy(repo.id);
     $scope.repo = angular.copy(repo.title);
     $scope.repo = angular.copy(repo.author);
   };
 
   }
-  //submit的逻辑： 1.根据select的选项 将input的值update进去 2.点击submit 获取更新后的json文件
   // $scope.submit = function () {
   //   $state.go('xxxxxxxx')
     // const id = $scope.id;
