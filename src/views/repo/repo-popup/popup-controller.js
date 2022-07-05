1faba4522e7d7bab76c81a8679f1c2b3a2118623
hanshen <han.shen@thoughtworks.com>
2022-07-05 17:32:03 +0800

    change: redesign popup table.

--- src/views/repo/repo-popup/popup-controller.js
+++ src/views/repo/repo-popup/popup-controller.js
@@ -1,45 +1,50 @@
-const Controller = function ($scope, $uibModalInstance, repo) {
+const Controller = function ($scope, $uibModalInstance, $state, repo) {
   $scope.repo = repo;
   $scope.id = repo.id;
   $scope.title = repo.title;
   $scope.author = repo.author;
+  // $scope.selectedOption = $scope.id;
 
   $scope.cancel = function () {
-    $uibModalInstance.dismiss('cancel');
+    $uibModalInstance.close();
   };
 
-  $scope.changedContent = function (input){
+  $scope.update = function (inputContent){
 
+    $scope.repo = angular.copy(inputContent);
+    $scope.repo = angular.copy(repo.title);
+    $scope.repo = angular.copy(repo.author);
   };
-
-  const updateUrl = '/repo/id';
-  // const data = $.param({
-  //   id: repo.id,
-  //   title: repo.title,
-  //   author: repo.author
-  // })
-  // const config = {
-  //   headers: {
-  //     'Content-Type': 'application/example'
-  //   }
-  // }
-
-  $scope.submit = function () {
-    const id = $scope.id;
-    const title = $scope.title;
-    const author = $scope.author;
-    this.updateRepos = () => $http.patch(updateUrl, data)
-    .then(function () {
-      $state.go('xxxxxxxx')
-    })
-
-    .error(function () {
-      alert('error');
-    })
 
   }
-}
+  //submit的逻辑： 1.根据select的选项 将input的值update进去 2.点击submit 获取更新后的json文件
+  // $scope.submit = function () {
+  //   $state.go('xxxxxxxx')
+    // const id = $scope.id;
+    // const title = $scope.title;
+    // const author = $scope.author;
+    // this.updateRepos = () => $http.patch(updateUrl, data)
+    // .then(function () {
+    //
+    // })
 
-  const popupController = ["$scope", "$uibModalInstance", "repo", Controller];
+    // .error(function () {
+    //   alert('error');
+    // })
+
+  // }
+
+// const updateUrl = '/repo/id';
+// const data = $.param({
+//   id: repo.id,
+//   title: repo.title,
+//   author: repo.author
+// })
+// const config = {
+//   headers: {
+//     'Content-Type': 'application/example'
+//   }
+// }
+  const popupController = ["$scope", "$uibModalInstance", "$state", "repo", Controller];
 
   export {popupController}
