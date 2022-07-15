c8e1b85af9541fd101151a40619229de5703547e
hanshen <han.shen@thoughtworks.com>
2022-07-15 10:54:19 +0800

    add: directive.

--- src/app-module.js
+++ src/app-module.js
@@ -3,6 +3,7 @@ import uiRouter from 'angular-ui-router'
 import uiBootstrap from 'angular-ui-bootstrap'
 import { appRouter } from "./app-router";
 import { repoService } from "./services/repo-service";
+import {starsDirective} from "./views/repo/stars.directive";
 
 const appModule = angular.module('app', [ uiRouter, uiBootstrap ]);
 
@@ -10,6 +11,6 @@ appModule.config(appRouter);
 
 appModule.service("repoService", repoService);
 
-appModule.directive("stars",)
+appModule.directive("stars", starsDirective)
 
 export { appModule }
--- src/views/repo/stars.directive.js
+++ src/views/repo/stars.directive.js
@@ -1,51 +1,27 @@
-// const Controller = function ($scope,repo){
-//   $scope.repo = _.cloneDeep(repo)
-//   console.log('sr',$scope.repo)
-// }
-//
-// const stars = function directive(){
-//   const directive = {};
-//   directive.restrict = 'EA';
-//   directive.template = "{{repo.stars}}";
-//   directive.compile = function (){
-//     return function ($scope, element) {
-//       if ($scope.repo.stars === 0)
-//         element.css("background-color", "#808080");
-//       if (100 > $scope.repo.stars && $scope.repo.stars > 0)
-//         element.css("background-color", "#008000");
-//       if (1000 > $scope.repo.stars && $scope.repo.stars >= 100)
-//         element.css("background-color", "#ffA500");
-//       if ($scope.repo.stars >= 1000)
-//         element.css("background-color", "#ff0000");
-//     };
-//   }
-//   return directive;
-//
-// }
-
-
-// ------
-
-angular
-.module("app", [])
-.directive('stars',directive);
-
-function directive(){
+const stars = function directive() {
   return {
-    restrict :'EAC',
-    scope:{
-      repo:'='
+    restrict: 'EAC',
+    scope: {
+      value: '='
     },
-    template : "{{repo.stars}}",
-    link: function ($scope,element) {
-      if ($scope.repo.stars === 0)
+    template: "{{value}}",
+    link: function ($scope, element) {
+      if ($scope.value === 0) {
         element.css("background-color", "#808080");
-      if (100 > $scope.repo.stars && $scope.repo.stars > 0)
+      }
+      if (100 > $scope.value && $scope.value > 0) {
         element.css("background-color", "#008000");
-      if (1000 > $scope.repo.stars && $scope.repo.stars >= 100)
+      }
+      if (1000 > $scope.value && $scope.value >= 100) {
         element.css("background-color", "#ffA500");
-      if ($scope.repo.stars >= 1000)
+      }
+      if ($scope.value >= 1000) {
         element.css("background-color", "#ff0000");
+      }
     }
   }
 }
+
+const starsDirective = [stars]
+
+export {starsDirective}
